1f60f3e508f5c5ed4ea096c059b2e6a46b683630
yuyaozawaa <yuyaozawa0123@gmail.com>
2023-08-18 16:42:12 +0900

    Initial commit

---
+++ app/src/main/java/jp/yuya/ozawa/subscriptionapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package jp.yuya.ozawa.subscriptionapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
